8b58638c26e75b59c1fda1be15974bf7958eb407
Anjana-JS <anjanajs2462@gmail.com>
2023-04-24 20:45:13 +0530

    added quadratic solution

---
+++ Quadratic.py
@@ -0,0 +1,9 @@
+a = int(input("enter value of a"))
+b = int(input("enter value of b"))
+c = int(input("enter value of c"))
+k = (b**2)-(4*a*c)
+d = k**0.5
+x1 = (-b+d)/(2*a)
+x2 = (-b-d)/(2*a)
+print(x1,x2)
+
