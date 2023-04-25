7d9062e9aaa354e9679547d6c7c4f83eeda04386
Anjana-JS <anjanajs2462@gmail.com>
2023-04-25 21:31:28 +0530

    Figuring out a leap year

--- leap year.py
+++ leap year.py
@@ -2,11 +2,11 @@
 
 def ly(year):
     if year%4 != 0:
-        print("Ah!","not a leap year")
-    elif year%100 ==0:
-        if year%400 == 0:
-            print("It's a leap year")
-        else:
+        print("Ah!Definitely not a leap year")
+    elif year%4 ==0:
+        if year%100 !=0 | year%400 != 0:
             print("Not a leap year")
+        else:
+            print("It's a leap year")
 year = int(input("Enter a year, please"))
 ly(year)
